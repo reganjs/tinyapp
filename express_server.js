c9145ee5a28533e64cc12f857510568cbabb3504
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-03 15:21:46 -0700

    Added the GET route for urls_new and POST route

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlEncoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,9 +11,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -22,13 +37,9 @@ app.get("/urls/:shortURL", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World!</b><body></html>\n"); 
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+const generateRandomString = function () {
+  
+};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
