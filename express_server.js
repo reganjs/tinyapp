0a6eb72a75e555b1dba4bad23f0944dbac76c0c4
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-03 14:06:29 -0700

    Added GET request to pass data to urls_index.js

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World!</b><body></html>\n"); 
 });
